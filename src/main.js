333cbdfefe47c84656651bbc768408afe3541668
igord1602 <63960745+igord1602@users.noreply.github.com>
2021-01-10 21:16:08 +0300

    Delete main.js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import configureRouter from "./routerConfig.js";
-
-export function startApplication(doc) {
-  console.log("TODO Application started");
-
-  const router = configureRouter(doc, "/");
-
-  router.navigate("/");
-}
